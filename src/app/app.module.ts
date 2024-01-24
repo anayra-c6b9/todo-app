19d3f29be50106830db908ab7933aeea88d92dd7
anayra-c6b9 <anayra13@proton.me>
2024-01-24 18:22:14 +0530

    Added raw structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppContentComponent } from './app-content/app-content.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppContentComponent,
+    SearchBarComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
